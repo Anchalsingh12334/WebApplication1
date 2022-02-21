c21de2050d10d8c65f6ae5f90bc1cbc24439205c
dell <dell@DESKTOP-7AQICT6>
2022-02-21 05:43:44 +0530

    Add project files.

---
+++ WebApplication1/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebApplication1.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Mobile { get; set; }
+        public String Location { get; set; }
+    }
+}
+
